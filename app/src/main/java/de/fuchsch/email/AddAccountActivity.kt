06817696e64cc782153c8ee7edea4a59c0d1d884
cf <abtjh5q@hmamail.com>
2019-11-03 11:41:08 +0100

    Implement add account button on add account activity

--- app/src/main/java/de/fuchsch/email/AddAccountActivity.kt
+++ app/src/main/java/de/fuchsch/email/AddAccountActivity.kt
@@ -1,14 +1,23 @@
 package de.fuchsch.email
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import de.fuchsch.email.database.entity.Account
+import de.fuchsch.email.database.entity.AccountSetting
+import de.fuchsch.email.database.entity.IMAPProtocol
+import de.fuchsch.email.viewmodel.AccountsViewModel
 
 import kotlinx.android.synthetic.main.activity_add_account.*
 import kotlinx.android.synthetic.main.content_add_account.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AddAccountActivity : AppCompatActivity() {
+
+    private val TAG = this::class.qualifiedName
+
+    private val model: AccountsViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +43,19 @@ class AddAccountActivity : AppCompatActivity() {
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             SMTPSpinner.adapter = adapter
         }
+
+        AddAccountButton.setOnClickListener{ addAccount() }
+    }
+
+    private fun addAccount() {
+        val settings = AccountSetting(ServerURLEdit.text?.toString() ?: "",
+            EMailEdit.text?.toString() ?: "",
+            PasswordEdit.text?.toString() ?: "",
+            IMAPProtocol.IMAPS)
+        val account = Account(NameEdit.text?.toString() ?: "", settings)
+        Log.i(TAG, "Adding account: $account")
+        model.insert(account)
+        setResult(0)
     }
 
     override fun onSupportNavigateUp(): Boolean {
