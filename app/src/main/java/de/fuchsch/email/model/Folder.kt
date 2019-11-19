f1532b2a7d781b648a5b71ecc6288f1259d87597
cf <abtjh5q@hmamail.com>
2019-11-19 13:29:27 +0100

    Make folder parcelable

--- app/src/main/java/de/fuchsch/email/model/Folder.kt
+++ app/src/main/java/de/fuchsch/email/model/Folder.kt
@@ -1,12 +1,15 @@
 package de.fuchsch.email.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import javax.mail.Folder
 
+@Parcelize
 data class Folder (
     val name: String,
     val messageCount: Int,
     val hasUnreadMessages: Boolean
-) {
+): Parcelable {
 
     companion object {
 
