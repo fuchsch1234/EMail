999ca44912a30b5ab096595a234f99c74788c965
cf <abtjh5q@hmamail.com>
2019-12-01 11:01:07 +0100

    Add tests for deleting and moving of message

--- app/src/test/java/de/fuchsch/email/MessageActivityUnitTest.kt
+++ app/src/test/java/de/fuchsch/email/MessageActivityUnitTest.kt
@@ -1,25 +1,49 @@
 package de.fuchsch.email
 
 import android.content.Intent
+import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.nhaarman.mockitokotlin2.*
 import de.fuchsch.email.activity.MessageActivity
 import de.fuchsch.email.model.Message
+import de.fuchsch.email.viewmodel.MessageViewModel
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.mock.declareMock
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
-class MessageActivityUnitTest {
+class MessageActivityUnitTest: KoinTest {
 
     private val message = Message("Test subject",
         "Test message",
         "sender@example.com",
         listOf("recipient@example.com", "cc@example.com"))
+
+    private lateinit var viewModel: MessageViewModel
+
+    @Before
+    fun setUp() {
+        declareMock<MessageViewModel> {
+            viewModel = this
+            given(message).willReturn { MutableLiveData(this@MessageActivityUnitTest.message) }
+        }
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
 
     @Test
     fun `message is displayed`() {
@@ -35,4 +59,27 @@ class MessageActivityUnitTest {
         onView(withId(R.id.MessageText)).check(matches(withText(message.message)))
     }
 
+    @Test
+    fun `message can be moved`() {
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            MessageActivity::class.java)
+        intent.putExtra(MessageActivity.MESSAGE, message)
+        ActivityScenario.launch<MessageActivity>(intent)
+
+        onView(withId(R.id.MessageMoveButton)).perform(click())
+        verify(viewModel,  times(1)).moveMessage(any())
+    }
+
+    @Test
+    fun `message can be deleted`() {
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(),
+            MessageActivity::class.java)
+        intent.putExtra(MessageActivity.MESSAGE, message)
+        ActivityScenario.launch<MessageActivity>(intent)
+
+        onView(withId(R.id.MessageDeleteButton)).perform(click())
+        verify(viewModel, times(1)).deleteMessage()
+    }
 }
