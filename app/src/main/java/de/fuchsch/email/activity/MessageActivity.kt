84777b47f8e498c37642eb03ee93111ddd210c72
cf <abtjh5q@hmamail.com>
2019-12-01 11:00:35 +0100

    Use viewmodel in message activity

--- app/src/main/java/de/fuchsch/email/activity/MessageActivity.kt
+++ app/src/main/java/de/fuchsch/email/activity/MessageActivity.kt
@@ -2,11 +2,14 @@ package de.fuchsch.email.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import de.fuchsch.email.R
 import de.fuchsch.email.model.Message
+import de.fuchsch.email.viewmodel.MessageViewModel
 
 import kotlinx.android.synthetic.main.activity_message.*
 import kotlinx.android.synthetic.main.content_message.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MessageActivity : AppCompatActivity() {
 
@@ -16,6 +19,8 @@ class MessageActivity : AppCompatActivity() {
 
     }
 
+    private val viewModel: MessageViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
@@ -24,12 +29,21 @@ class MessageActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
         }
 
+        viewModel.message.observe(this, Observer {
+            it?.let { message ->
+                SubjectLineText.setText(message.subject)
+                SenderText.setText(message.sender)
+                RecipientText.setText(message.recipients.first())
+                MessageText.setText(message.message)
+            }
+        })
+
         intent.getParcelableExtra<Message>(MESSAGE).also { message ->
-            SubjectLineText.setText(message.subject)
-            SenderText.setText(message.sender)
-            RecipientText.setText(message.recipients.first())
-            MessageText.setText(message.message)
+            viewModel.setMessage(message)
         }
+
+        MessageDeleteButton.setOnClickListener { viewModel.deleteMessage() }
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
